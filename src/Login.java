8c13a585909969873e0f05aee78b30597885268d
trieunhuyenquoc <trieunguyenquoc020604@gmai.com>
2023-02-17 15:30:09 +0700

    https://github.com/TrieuNguyenQuoc/CaseModule2

---
+++ src/Login.java
@@ -0,0 +1,22 @@
+public class Login {
+    public boolean loGinUserName(){
+        System.out.println("xin mời nhập tên đang nhập");
+        String username1=scanner.nextLine();
+        if (username1.equalsIgnoreCase(username)){
+            return true;
+        }else return false;
+    }public boolean loginPassword(){
+        System.out.println("nhập mật khẩu");
+        int password1=scanner.nextInt();
+        if (password1==password){
+            return true;
+        }else return false;
+    }public void checkLogin(){
+        if (loGinUserName()&&loginPassword()){
+            managerLecturers.menuManager();
+        }else {
+            System.out.println("xin mời đăng nhập lại");
+            checkLogin();
+        }
+    }
+}
